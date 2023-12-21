fa67b317bccb657358bfe0a00cd6a691f6e36f5c
Носак Вячеслав <v.nosak@asna.ru>
2023-12-21 22:48:34 +0300

    add message

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,11 +8,12 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
+
         Configuration.pageLoadStrategy = "eager";
-        
+        //TEST1
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
-
+        //TEST2
     }
 }
